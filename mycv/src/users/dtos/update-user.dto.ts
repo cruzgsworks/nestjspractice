ebbcdff2792e503f4519447143775bc6e1cfdd6f
Gerard Cruz <Gerard.Igmedio@cognizant.com>
2023-02-13 14:07:10 -0600

    before custom interceptors nestjs

---
+++ mycv/src/users/dtos/update-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsString, IsOptional } from 'class-validator'
+
+export class UpdateUserDTO {
+    
+    @IsEmail()
+    @IsOptional()
+    email: string;
+
+    @IsString()
+    @IsOptional()
+    password: string;
+}
