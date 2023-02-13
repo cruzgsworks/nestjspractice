bfa76784b15a467de79caa23c1bf2dc048042600
Gerard Cruz <Gerard.Igmedio@cognizant.com>
2023-02-13 14:11:56 -0600

    Revert "before custom interceptors nestjs"
    This reverts commit f17fc11324ea2c27ea0451c131c3d356812fafb5.

--- mycv/src/users/user.entity.ts
+++ mycv/src/users/user.entity.ts
@@ -1,38 +1,29 @@
-import { Exclude } from 'class-transformer';
-import {
-  AfterInsert,
-  AfterRemove,
-  AfterUpdate,
-  Entity,
-  Column,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { AfterInsert, AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn } from "typeorm"
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @Column()
-  email: string;
+    @Column()
+    email: string;
 
-  @Column()
-  @Exclude()
-  password: string;
+    @Column()
+    password: string;
 
-  // hook declarators
-  @AfterInsert()
-  logInsert() {
-    console.log('Inserted User with id', this.id);
-  }
+    // hook declarators
+    @AfterInsert()
+    logInsert() {
+        console.log('Inserted User with id', this.id)
+    }
 
-  @AfterUpdate()
-  logUpdate() {
-    console.log('Updated User with id', this.id);
-  }
+    @AfterUpdate()
+    logUpdate() {
+        console.log('Updated User with id', this.id)
+    }
 
-  @AfterRemove()
-  logRemove() {
-    console.log('Removed User with id', this.id);
-  }
+    @AfterRemove()
+    logRemove() {
+        console.log('Removed User with id', this.id)
+    }
 }
