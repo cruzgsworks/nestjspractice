f07f623c66921ebbd634ffa548221f0afd04e87d
Gerard Cruz <Gerard.Igmedio@cognizant.com>
2023-02-13 14:27:43 -0600

    revert

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,15 +1,51 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Query,
+  Param,
+  Delete,
+  NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor
+} from '@nestjs/common';
 import { CreateUserDTO } from 'src/users/dtos/create-user.dto';
+import { UpdateUserDTO } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
 
-    constructor(private usersService: UsersService) {}
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDTO) {
+    this.usersService.create(body.email, body.password);
+  }
 
-    @Post('/signup')
-    createUser(@Body() body: CreateUserDTO) {
-        this.usersService.create(body.email, body.password);
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('user not found');
     }
+    return user;
+  }
 
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDTO) {
+    return this.usersService.update(parseInt(id), body);
+  }
 }
