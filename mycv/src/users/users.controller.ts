58d9c3982456fbf28b6d63d1fc013e4bc143d737
Gerard Cruz <Gerard.Igmedio@cognizant.com>
2023-02-12 21:06:20 -0600

    update

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDTO } from 'src/users/dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+
+    constructor(private usersService: UsersService) {}
+
+    @Post('/signup')
+    createUser(@Body() body: CreateUserDTO) {
+        this.usersService.create(body.email, body.password);
+    }
+
+}
