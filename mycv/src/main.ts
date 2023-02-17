96a25bdfb13ce0c5aaa24ec052eb8c8a3185065a
Gerard Cruz <Gerard.Igmedio@cognizant.com>
2023-02-17 15:19:04 -0600

    112

--- mycv/src/main.ts
+++ mycv/src/main.ts
@@ -1,18 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { setupApp } from './setup-app';
 const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys: ['randomstring']
-  }));
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // small security feature
-    })
-  );
-  await app.listen(3000);
+  
+  // app.use(cookieSession({
+  //   keys: ['randomstring']
+  // }));
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true, // small security feature
+  //   })
+  // );
+  // await app.listen(3000);
+
+  setupApp(app);
 }
 bootstrap();
---
+++ mycv/test/auth.e2e-spec.ts
@@ -0,0 +1,32 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { setupApp } from './../src/setup-app';
+
+describe('Authentication System (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication(); 
+    setupApp(app);
+    await app.init();
+  });
+
+  it('Handles a signup request', () => {
+    const newEmail = 'asdfg432@asdfg.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({email: newEmail, password: 'asdfg'})
+      .expect(201)
+      .then((res) => {
+        const {id, email} = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(newEmail);
+      })
+  });
+});
