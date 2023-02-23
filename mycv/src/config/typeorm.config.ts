b889fc705c30e481c34036644c142088346ef763
Gerard Cruz <Gerard.Igmedio@cognizant.com>
2023-02-23 14:04:02 -0600

    finish

--- mycv/src/config/typeorm.config.ts
+++ mycv/src/config/typeorm.config.ts
@@ -5,12 +5,51 @@ import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) {}
+
+  isTrueSet(value: string) {
+    return (value.toLowerCase() === 'true');
+  }
+
   createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
-    return {
-      type: 'sqlite',
-      synchronize: false,
-      database: this.configService.get<string>('DB_NAME'),
-      autoLoadEntities: true,
-    };
+    let dbConfig = {} as TypeOrmModuleOptions;
+    switch (process.env.NODE_ENV) {
+      case 'development':
+        Object.assign(dbConfig, {
+          type: this.configService.get<string>('DB_TYPE'),
+          synchronize: this.isTrueSet(this.configService.get<string>('DB_SYNCHRONIZE')),
+          database: this.configService.get<string>('DB_NAME'),
+          autoLoadEntities: this.isTrueSet(this.configService.get<string>('DB_AUTOLOAD_ENTITIES')),
+        });
+        break;
+      case 'test':
+        Object.assign(dbConfig, {
+          type: this.configService.get<string>('DB_TYPE'),
+          synchronize: this.isTrueSet(this.configService.get<string>('DB_SYNCHRONIZE')),
+          database: this.configService.get<string>('DB_NAME'),
+          autoLoadEntities: this.isTrueSet(this.configService.get<string>('DB_AUTOLOAD_ENTITIES')),
+          migrationsRun: this.isTrueSet(this.configService.get<string>('DB_MIGRATIONS_RUN')),
+          entities: ['**/*.entity.ts'],
+          migrations: ['src/migrations/*{.js,.ts}'],
+        });
+        break;
+      case 'production':
+        Object.assign(dbConfig, {
+          type: this.configService.get<string>('DB_TYPE'),
+          synchronize: this.isTrueSet(this.configService.get<string>('DB_SYNCHRONIZE')),
+          username: this.configService.get<string>('DB_USERNAME'),
+          password: this.configService.get<string>('DB_PASSWORD'),
+          autoLoadEntities: this.isTrueSet(this.configService.get<string>('DB_AUTOLOAD_ENTITIES')),
+          url: this.configService.get<string>('DB_URL'),
+          migrationsRun: this.isTrueSet(this.configService.get<string>('DB_MIGRATIONS_RUN')),
+          entities: ['**/*.entity.js'],
+          migrations: ['dist/migrations/*{.js,.ts}'],
+          ssl: this.isTrueSet(this.configService.get<string>('DB_SSL')),
+        });
+        break;
+      default:
+        throw new Error('unknown environment');
+    }
+    console.log(dbConfig);
+    return dbConfig;
   }
 }
--- mycv/src/data-source.ts
+++ mycv/src/data-source.ts
@@ -3,5 +3,5 @@ export const appDataSource = new DataSource({
   type: 'sqlite',
   database: 'db.sqlite',
   entities: ['**/*.entity.ts'],
-  migrations: [__dirname + '/migrations/*.ts'],
+  migrations: [__dirname + '/migrations/*{.js,.ts}'],
 } as DataSourceOptions);
